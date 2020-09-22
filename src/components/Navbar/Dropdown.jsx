3cde609e04054ea84a889d65d631a2358933bfe7
Басун Володимир <vbasoon@gmail.com>
2020-09-22 09:06:58 +0300

    dropdown jsx begin

--- src/components/Navbar/Dropdown.jsx
+++ src/components/Navbar/Dropdown.jsx
@@ -1,6 +1,28 @@
-import React, { useState } from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
-import Dropdown from './Dropdown'
+import React, { useState } from 'react';
+import { MenuItems } from './MenuItems';
+import { Link } from 'react-router-dom';
+import './Dropdown.css';
+
+function Dropdown() {
+   const [click, setClick] = useState(false)
+
+   const handleClick = () => setClick(!click)
+
+   return (
+      <div>
+         <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+         </ul>
+      </div>
+   );
+}
+
+export default Dropdown
+
+
+
 
